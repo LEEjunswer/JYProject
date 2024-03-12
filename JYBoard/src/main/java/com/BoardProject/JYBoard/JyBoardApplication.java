931bea89c88590351faf7a208253b3c34da82054
LEEjunswer <jun_5949@naver.com>
2024-03-12 10:22:15 +0900

    애니-피규어 커뮤니티 게시판 시작

---
+++ JYBoard/src/main/java/com/BoardProject/JYBoard/JyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.BoardProject.JYBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyBoardApplication.class, args);
+	}
+
+}
